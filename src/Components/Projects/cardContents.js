7fa3c225f0d8f3e2b7e164e9cdc67ff13fa107ad
NirmalHegde <nirmalhegde821@gmail.com>
2021-03-07 22:02:47 -0500

    logic comments

--- src/Components/Projects/cardContents.js
+++ src/Components/Projects/cardContents.js
@@ -1,7 +1,9 @@
 export const cardContents = [
 	{
+		header: "",
 		title: "title1",
-		details: "description1"
+		details: "description1",
+		source: "https://nirmalhegde.github.io/Virtual-Wonders/"
 	},
 	{
 		title: "title2",
@@ -37,21 +39,24 @@ export const cardContents = [
 	},
 ];
 
+// init for populating split arrays
 const cardNumber = 3;
 let firstSectionInit = [];
 let secondSectionInit = [];
 let thirdSectionInit = [];
 
+// populate arrays for each section
 for (let i = 0; i < cardContents.length; i++) {
 	firstSectionInit.push(cardContents[i * cardNumber]);
 	secondSectionInit.push(cardContents[i * cardNumber + 1]);
 	thirdSectionInit.push(cardContents[i * cardNumber + 2]);
 
-	if (i * cardNumber + 3 === cardContents.length) {
+	if (i * cardNumber + 3 >= cardContents.length) {
 		break;
 	}
 }
 
+// export arrays for each section
 export const firstSectionCards = firstSectionInit;
 export const secondSectionCards = secondSectionInit;
 export const thirdSectionCards = thirdSectionInit;
